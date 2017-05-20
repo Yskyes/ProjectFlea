3d604db3d09c110dd613f885ae85e7d92f583d34
Rjacobs15 <robin.eetu.jacobs@gmail.com>
2017-05-20 16:33:25 +0300

    Title and description search functionality added
    One can now search for entries by partial matches of username, title,
    description in any combination or lack thereof.

--- testfiles/search.php
+++ testfiles/search.php
@@ -1,54 +1,47 @@
 <?php
 	require_once 'connection.php';
 	
-    	// Get values passed from the html page
-//	$searchtitle = $_POST['searchtitle'];
+	//get values passed from the html page
+	$searchtitle = $_POST['searchtitle'];
 	$searchuser = $_POST['searchuser'];
 //	$searchcategory = $_POST['searchcategory'];
 //	$searchlocation = $_POST['searchlocation'];
-//	$searchdescription = $_POST['searchdescription'];
+	$searchdescription = $_POST['searchdescription'];
 
 	//creates a prepared statement and binds variables as parameters
-//	$stmt = $connection->prepare("SELECT * FROM advertisments WHERE title LIKE ? AND username LIKE ? AND category_id = ? AND location_id = ? AND description LIKE ?");
+//	$stmt = $connection->prepare("SELECT * FROM advertisements WHERE title LIKE ? AND username LIKE ? AND categoryid = ? AND locationid = ? AND description LIKE ?");
 //	$stmt->bind_param("ssiis", $searchtitle, $searchuser, $searchlocation, $searchcategory, $searchdescription);
-	$stmt = $connection->prepare("SELECT * FROM advertisements WHERE username LIKE ?");
-	$stmt->bind_param("s", $searchuser);
+	$stmt = $connection->prepare("SELECT * FROM advertisements WHERE title LIKE ? AND username LIKE ? AND description LIKE ?");
+	$stmt->bind_param("sss", $searchtitle, $searchuser, $searchdescription);
+	
+	//allow for partial matches. TODO: allow user to specify whether they want partial or full matches
+	$searchtitle = '%' . $searchtitle . '%';
+	$searchuser = '%' . $searchuser . '%';
+	$searchdescription = '%' . $searchdescription . '%';
 
-	// Query the database 
+	//execute query
 	$result = $stmt->execute();
 
-   /* Get the result */
-   $result = $stmt->get_result();
+	//get the result
+	$result = $stmt->get_result();
    
-   /* Get the number of rows */
-   $num_of_rows = $result->num_rows;
-   
-      while ($row = $result->fetch_assoc()) {
-        echo 'title: '.$row['title'].'<br>';
-        echo 'username: '.$row['username'].'<br>';
-        echo 'categoryid: '.$row['categoryid'].'<br>';
-        echo 'locationid: '.$row['locationid'].'<br>';
+	//get the number of rows
+	$num_of_rows = $result->num_rows;
+	
+	//print it all out
+	while ($row = $result->fetch_assoc()) {
+		echo 'title: '.$row['title'].'<br>';
+		echo 'username: '.$row['username'].'<br>';
+		echo 'pricerequest: '.$row['pricerequest'].'<br>';
+		echo 'leftdate: '.$row['leftdate'].'<br>';
+		echo 'categoryid: '.$row['categoryid'].'<br>';
+		echo 'locationid: '.$row['locationid'].'<br>';
 		echo 'description: '.$row['description'].'<br><br>';
-   }
+	}
 
-   /* free results */
+   //free results
    $stmt->free_result();
 
-
-	// Places result line in an array and prints
-//	while ($row = mysqli_fetch_array($result))
-//	{
-//	echo 'id: ' .$row['id'];
-//	echo '<br /> title: ' .$row['title'];
-//	echo '<br /> user_username: ' .$row['user_username'];
-//	echo '<br /> category_id: ' .$row['category_id'];
-//	echo '<br /> location_id: ' .$row['location_id'];
-//	echo '<br /> description: ' .$row['description'];
-//	echo '<br /> pricerequest: ' .$row['pricerequest'];
-//	echo '<br /> picture: ' .$row['picture'];
-//	echo '<br /> leftdate: ' .$row['leftdate'];
-//	}
-
-	$stmt->close();
+$stmt->close();
 
 ?>	
