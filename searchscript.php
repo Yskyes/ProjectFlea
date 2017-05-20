7c9c815d165ccd800284930e82995dce7d5d44ac
Rjacobs15 <robin.eetu.jacobs@gmail.com>
2017-05-20 17:31:55 +0300

    Search results layout, file rename and move
    Search results now have overall site layout. Moved script into main
    folder and renamed

--- searchscript.php
+++ searchscript.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="online second-hand shopping platform!">
+	<meta name="keywords" content="shopping, online, second-hand, fleamarket, HTML5, CSS">
+	<meta name="authors" content="Robin Jacobs, Mikko Jaakonsaari">
+	<link rel = "stylesheet" type = "text/css" href = "stylesheet.css">
+	<title>Flea</title>
+</head>
+
+<body>
+	<?php
+		include 'header.php';
+	?>
+	<?php
+		include 'nav.php';
+	?>
+	<?php
+		include 'footer.php';
+	?>
+</body>
+</html>
 <?php
 	require_once 'connection.php';
 	
@@ -44,4 +67,4 @@
 
 $stmt->close();
 
-?>	
+?>
