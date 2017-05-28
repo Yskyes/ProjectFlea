57d8d6ad1a49b2b42d8a10422fd033607038814d
Yskyes <mikko.jaakonsaari@live.fi>
2017-05-28 12:54:32 +0300

    Front page  now presents 10 latest entries

--- front.php
+++ front.php
@@ -25,7 +25,31 @@
 		<p>Flea is the bass player for american funk-rock band Red Hot Chili Peppers. But in this context, Flea is a school project to create an online fleamarket / second-hand store. </p>
 		<p>In Flea, users can search for products other people are selling, or set up a new ad themselves. </p>
 		<p>Here are some of the ads that have just popped up on the site:</p>
-		<p>(They'd pop up here)</p>
+	<?php
+		$showten = "SELECT title, leftdate, id, pricerequest FROM advertisements ORDER BY leftdate ASC LIMIT 10";
+
+		// Query the database 
+		$result = mysqli_query($connection, $showten);
+
+		
+		//print it all out
+		$table="<table>
+						<tr>
+							<th>Title</th>
+							<th>Price Request</th>
+							<th>Left Date</th>
+						</tr>";
+
+		while ($row = $result->fetch_assoc()) 
+		{
+			$entryaddress = "entryview.php?entry=".$row['id'];
+			$table.= "<tr>  <td><u> <a href=".$entryaddress.">".$row['title']."</a></u> </td> <td>".$row['pricerequest']."</td> <td>".$row['leftdate']."</td></tr>";
+								
+		}
+		$table.="</table>";
+		echo $table;
+					
+	?>
 	<?php
 		include 'footer.php';
 	?>
